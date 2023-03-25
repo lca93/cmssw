eb13203cff05a687e6ec6957c31f3681ffb45402
AdrianoDee <adriano.diflorio@ba.infn.it>
2023-03-25 13:39:12 +0100

    Enabling a simple Patatrack seeded iteration

--- HLTrigger/Configuration/python/customizePhase2HLTTracking.py
+++ HLTrigger/Configuration/python/customizePhase2HLTTracking.py
@@ -2,8 +2,8 @@ import FWCore.ParameterSet.Config as cms
 # validation & dqm modules 
 from HLTrigger.Configuration.phase2TrackingValidation_cff import *
 
-def customisePhase2HLTForTrackingOnly(process):
-    
+def addTrackingValidation(process):
+
     process.TrackMon_gentk  = TrackMon_gentk.clone()
     process.TrackSplitMonitor = TrackSplitMonitor.clone()
     process.TrackerCollisionSelectedTrackMonCommongeneralTracks = TrackerCollisionSelectedTrackMonCommongeneralTracks.clone()
@@ -34,21 +34,6 @@ def customisePhase2HLTForTrackingOnly(process):
     #The vertex validator
     process.vertexAnalysis = vertexAnalysis.clone()
 
-    #Baseline tracking path
-    process.HLTTrackingV61Path = cms.Path(process.HLTTrackingV61Sequence)
-
-    process.RawToDigiTask = cms.Task(process.ecalDigisTask, process.ecalPreshowerDigis, process.hcalDigis, process.hgcalDigis, process.muonCSCDigis, process.muonDTDigis, process.muonGEMDigis)
-    process.calolocalrecoTask = cms.Task(process.hcalGlobalRecoTask,process.ecalLocalRecoTask, process.hcalLocalRecoTask)
-
-    process.hrecoTask = cms.Task(process.RawToDigiTask, process.calolocalrecoTask)
-    process.localSeq = cms.Sequence(process.hrecoTask) #For the moment no MTD,process.mtdRecoTask)
-    process.localPath = cms.Path(process.localSeq)
-
-    process.vertexRecoTask = cms.Task(process.ak4CaloJetsForTrk, process.initialStepPVTask, process.offlinePrimaryVertices, process.trackRefsForJetsBeforeSorting, process.trackWithVertexRefSelectorBeforeSorting, process.unsortedOfflinePrimaryVertices,process.goodOfflinePrimaryVertices)
-
-    process.vertexRecoSeq = cms.Sequence(process.vertexRecoTask) ## No MTD : ,process.vertex4DrecoTask)
-    process.vertexRecoPath = cms.Path(process.vertexRecoSeq)
-
     #DQM
     process.DQMoutput = cms.OutputModule("DQMRootOutputModule",
         dataset = cms.untracked.PSet(
@@ -68,19 +53,194 @@ def customisePhase2HLTForTrackingOnly(process):
     process.tracksValidationTruth = cms.Task(process.VertexAssociatorByPositionAndTracksPixel,process.VertexAssociatorByPositionAndTracks, process.quickTrackAssociatorByHits, process.tpClusterProducer, process.trackingParticleNumberOfLayersProducer, process.trackingParticleGeneralTrackAssociation,process.trackingParticlePixelTrackAssociation)
     process.validation = cms.Sequence(process.tracksValidation,process.tracksValidationTruth)
     process.validation_step = cms.EndPath(process.validation)
+
+    process.schedule.extend([process.validation_step,
+        process.dqm_step,
+        process.DQMoutput_step])
     
+    return process
+
+def customisePhase2HLTForTrackingOnly(process):
+    
+    #Baseline tracking path
+    process.HLTTrackingV61Path = cms.Path(process.HLTTrackingV61Sequence)
+
+    process.RawToDigiTask = cms.Task(process.ecalDigisTask, process.ecalPreshowerDigis, process.hcalDigis, process.hgcalDigis, process.muonCSCDigis, process.muonDTDigis, process.muonGEMDigis)
+    process.calolocalrecoTask = cms.Task(process.hcalGlobalRecoTask,process.ecalLocalRecoTask, process.hcalLocalRecoTask)
+
+    process.hrecoTask = cms.Task(process.RawToDigiTask, process.calolocalrecoTask)
+    process.localSeq = cms.Sequence(process.hrecoTask) #For the moment no MTD,process.mtdRecoTask)
+    process.localPath = cms.Path(process.localSeq)
+
+    process.vertexRecoTask = cms.Task(process.ak4CaloJetsForTrk, process.initialStepPVTask, process.offlinePrimaryVertices, process.trackRefsForJetsBeforeSorting, process.trackWithVertexRefSelectorBeforeSorting, process.unsortedOfflinePrimaryVertices,process.goodOfflinePrimaryVertices)
+
+    process.vertexRecoSeq = cms.Sequence(process.vertexRecoTask) ## No MTD : ,process.vertex4DrecoTask)
+    process.vertexRecoPath = cms.Path(process.vertexRecoSeq)    
 
     ##Local Reco
     process.localPath = cms.Path(process.RawToDigiTask,process.localrecoTask)
 
     process.schedule = cms.Schedule(*[
-        #process.l1tReconstructionPath,
         process.localPath,
         process.HLTTrackingV61Path,
         process.vertexRecoPath,
-        process.validation_step,
-        process.dqm_step,
-        process.DQMoutput_step
-        ],tasks=[process.patAlgosToolsTask])
+        ])
         
     return process
+
+
+def customisePhase2HLTForPatatrack(process):
+
+    from HeterogeneousCore.CUDACore.SwitchProducerCUDA import SwitchProducerCUDA
+
+    from RecoLocalTracker.SiPixelRecHits.pixelCPEFastESProducerPhase2_cfi import pixelCPEFastESProducerPhase2
+    process.PixelCPEFastESProducerPhase2 = pixelCPEFastESProducerPhase2.clone()
+    ### SiPixelClusters on GPU
+
+    process.siPixelClustersLegacy = process.siPixelClusters.clone()
+
+    from RecoLocalTracker.SiPixelClusterizer.siPixelPhase2DigiToClusterCUDA_cfi import siPixelPhase2DigiToClusterCUDA as _siPixelPhase2DigiToClusterCUDA
+    process.siPixelClustersCUDA = _siPixelPhase2DigiToClusterCUDA.clone()
+    
+    from EventFilter.SiPixelRawToDigi.siPixelDigisSoAFromCUDA_cfi import siPixelDigisSoAFromCUDA as _siPixelDigisSoAFromCUDA
+    process.siPixelDigisPhase2SoA = _siPixelDigisSoAFromCUDA.clone(
+        src = "siPixelClusters"
+    )
+
+    from RecoLocalTracker.SiPixelClusterizer.siPixelDigisClustersFromSoAPhase2_cfi import siPixelDigisClustersFromSoAPhase2 as _siPixelDigisClustersFromSoAPhase2
+
+    process.siPixelClusters = SwitchProducerCUDA(
+        cpu = cms.EDAlias(
+            siPixelClustersLegacy = cms.VPSet(cms.PSet(
+                type = cms.string('SiPixelClusteredmNewDetSetVector')
+            ))
+            ),
+        cuda = _siPixelDigisClustersFromSoAPhase2.clone(
+            clusterThreshold_layer1 = 4000,
+            clusterThreshold_otherLayers = 4000,
+            src = "siPixelDigisPhase2SoA",
+            produceDigis = False
+            )
+    )
+
+    process.siPixelClustersTask = cms.Task(
+                            process.siPixelClustersLegacy,
+                            process.siPixelClustersCUDA,
+                            process.siPixelDigisPhase2SoA,
+                            process.siPixelClusters)
+    
+    ### SiPixel Hits
+
+    from RecoLocalTracker.SiPixelRecHits.siPixelRecHitCUDAPhase2_cfi import siPixelRecHitCUDAPhase2 as _siPixelRecHitCUDAPhase2
+    process.siPixelRecHitsCUDA = _siPixelRecHitCUDAPhase2.clone(
+        src = cms.InputTag('siPixelClustersCUDA'),
+        beamSpot = "offlineBeamSpotToCUDA"
+    )
+    from RecoLocalTracker.SiPixelRecHits.siPixelRecHitSoAFromLegacyPhase2_cfi import siPixelRecHitSoAFromLegacyPhase2 as _siPixelRecHitsSoAPhase2
+    process.siPixelRecHitsCPU = _siPixelRecHitsSoAPhase2.clone(
+        convertToLegacy=True, 
+        src = cms.InputTag('siPixelClusters'),
+        CPE = cms.string('PixelCPEFastPhase2'))
+
+    from RecoLocalTracker.SiPixelRecHits.siPixelRecHitSoAFromCUDAPhase2_cfi import siPixelRecHitSoAFromCUDAPhase2 as _siPixelRecHitSoAFromCUDAPhase2
+    process.siPixelRecHitsSoA = SwitchProducerCUDA(
+        cpu = cms.EDAlias(
+            siPixelRecHitsCPU = cms.VPSet(
+                 cms.PSet(type = cms.string("pixelTopologyPhase2TrackingRecHitSoAHost")),
+                 cms.PSet(type = cms.string("uintAsHostProduct"))
+             )),
+        cuda = _siPixelRecHitSoAFromCUDAPhase2.clone()
+
+    )
+
+    
+    from RecoLocalTracker.SiPixelRecHits.siPixelRecHitFromCUDAPhase2_cfi import siPixelRecHitFromCUDAPhase2 as _siPixelRecHitFromCUDAPhase2
+
+    _siPixelRecHits = SwitchProducerCUDA(
+        cpu = cms.EDAlias(
+            siPixelRecHitsCPU = cms.VPSet(
+                 cms.PSet(type = cms.string("SiPixelRecHitedmNewDetSetVector")),
+                 cms.PSet(type = cms.string("uintAsHostProduct"))
+             )),
+        cuda = _siPixelRecHitFromCUDAPhase2.clone(
+            pixelRecHitSrc = cms.InputTag('siPixelRecHitsCUDA'),
+            src = cms.InputTag('siPixelClusters'),
+        )
+    )
+
+    process.siPixelRecHits = _siPixelRecHits.clone()
+    process.siPixelRecHitsTask = cms.Task(
+        process.siPixelRecHitsCUDA,
+        process.siPixelRecHitsCPU,
+        process.siPixelRecHits,
+        process.siPixelRecHitsSoA
+        )
+
+    ### Pixeltracks
+
+    from RecoPixelVertexing.PixelTriplets.caHitNtupletCUDAPhase2_cfi import caHitNtupletCUDAPhase2 as _pixelTracksCUDAPhase2
+    process.pixelTracksCUDA = _pixelTracksCUDAPhase2.clone(
+        pixelRecHitSrc = "siPixelRecHitsCUDA",
+        idealConditions = False,
+        onGPU = True,
+    )
+
+    from RecoPixelVertexing.PixelTrackFitting.pixelTrackSoAFromCUDAPhase2_cfi import pixelTrackSoAFromCUDAPhase2 as _pixelTracksSoAPhase2
+    process.pixelTracksSoA = SwitchProducerCUDA(
+        # build pixel ntuplets and pixel tracks in SoA format on the CPU
+        cpu = _pixelTracksCUDAPhase2.clone(
+            pixelRecHitSrc = "siPixelRecHitsCPU",
+            idealConditions = False,
+            onGPU = False
+        ),
+        cuda = _pixelTracksSoAPhase2.clone()
+    )
+
+    from RecoPixelVertexing.PixelTrackFitting.pixelTrackProducerFromSoAPhase2_cfi import pixelTrackProducerFromSoAPhase2 as _pixelTrackProducerFromSoAPhase2
+    process.pixelTracks = _pixelTrackProducerFromSoAPhase2.clone(
+        pixelRecHitLegacySrc = "siPixelRecHits"
+    )
+
+    process.pixelTracksTask = cms.Task(
+        process.pixelTracksCUDA,
+        process.pixelTracksSoA,
+        process.pixelTracks
+    )
+
+    process.HLTTrackingV61Task = cms.Task(process.MeasurementTrackerEvent, 
+                                          process.generalTracks, 
+                                          process.highPtTripletStepClusters, 
+                                          process.highPtTripletStepHitDoublets, 
+                                          process.highPtTripletStepHitTriplets, 
+                                          process.highPtTripletStepSeedLayers, 
+                                          process.highPtTripletStepSeeds, 
+                                          process.highPtTripletStepTrackCandidates, 
+                                          process.highPtTripletStepTrackCutClassifier, 
+                                          process.highPtTripletStepTrackSelectionHighPurity, 
+                                          process.highPtTripletStepTrackingRegions, 
+                                          process.highPtTripletStepTracks, 
+                                          process.initialStepSeeds, 
+                                          process.initialStepTrackCandidates, 
+                                          process.initialStepTrackCutClassifier, 
+                                          process.initialStepTrackSelectionHighPurity, 
+                                          process.initialStepTracks, 
+                                          process.pixelVertices, ## for the moment leaving it as it was
+                                          )
+
+    process.trackerClusterCheckTask = cms.Task(process.trackerClusterCheck,
+                                               process.siPhase2Clusters, 
+                                               process.siPixelClusterShapeCache)
+    process.HLTTrackingV61Sequence = cms.Sequence(process.trackerClusterCheckTask,
+                                                  process.siPixelClustersTask,
+                                                  process.siPixelRecHitsTask,
+                                                  process.pixelTracksTask,
+                                                  process.HLTTrackingV61Task)
+    
+    return process
+
+
+
+    
+
+
+    
